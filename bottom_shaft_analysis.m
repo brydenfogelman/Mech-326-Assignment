5b528a2e1496c2e9ad4faadbfc72a245d1959c53
brydenfogelman <bryden1995@gmail.com>
2015-11-11 16:33:05 -0800

    Added some simple weight calculation for critical speeds

--- bottom_shaft_analysis.m
+++ bottom_shaft_analysis.m
@@ -8,6 +8,10 @@ A = 7; B = 11; C = 38;
 % Given
 k = 37.8 * 1000; % N/m
 L = 40; % shaft length
+d1 = 20/1000; % m
+d2 = 30/1000; % m
+bearing_width = 14/1000; % m
+g = 9.81; 
 
 % Creating position vector
 n_step = 1000;
@@ -40,3 +44,12 @@ critical_location = x((find(M==max_bending_moment))); % 40/1000 is derived from 
 %%%%%%%%%%%%%%%%%%%
 % CRITICAL SPEEDS %
 %%%%%%%%%%%%%%%%%%%
+
+density = 7.85*100^3/1000; % kg/m^3
+volume = (pi * (d1)^2/4)*(B/100+bearing_width/2) + (pi * (d2)^2/4)*((C-B)/100 - bearing_width) + (pi * (d1)^2/4)*((L-C)/100+bearing_width/2); % m^3
+weight = density*volume*g
+
+
+
+
+
